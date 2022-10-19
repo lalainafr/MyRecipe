ffe71a1daaededa6a97c84dfee49765df9e9f6e3
Lalaina RAJAONAHSOA <r.lalainafr@gmail.com>
2022-10-19 16:50:15 +0200

    login form - last_username & error on authentication - logout

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('pages/login/index.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+    }
+}
